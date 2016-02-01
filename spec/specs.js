ef81ea2cd2953c52a4b837201898b9fc2ba64c04
nic <netzeln@gmail.com>
2016-02-01 13:55:51 -0800

    pass spec test for exercise choice array

--- spec/specs.js
+++ spec/specs.js
@@ -9,33 +9,40 @@ describe('Exercise', function(){
 
 
 describe("Workout", function() {
-  it("will input the available time and equipment and output an exercise", function() {
-    var testExercise = new Exercise("Chair");
-    expect(testExercise.exerciseChooser()).to.equal("Chair step-up");
-  });
+  it("will insert an exercise object into the chosenExercise array", function(){
+    var testWorkout = new Workout ("[]");
 
-  it("will input the user's regimen choice and reveal the exercise description", function() {
-    var newWorkout = new newExercise("Standard Push-up");
-    expect(newWorkout.?????()).to.eql(["Places arms below shoulders in plank position, lower your body, keeping core engaged and body in a straight line, push back up to plank"]);
+    expect(testWorkout.chosenExercise).to.eql([]);
   });
+});
 
-  it("will input the user's name and greet them with said name", function() {
-    var newWorkout = new Workout("Torrence");
-    expect(newWorkout.userName).to.equal("Torrence");
-  });
-
-  it("will input available time and output number of exercises", function() {
-    var newWorkout = new Workout(10);
-    expect(newWorkout.????()).to.equal(5);
-  });
-
-  it("will input level of difficulty and output a matching regimen", function() {
-    var newWorkout = new Workout("Advanced");
-    expect(newWorkout.????()).to.eql(["Triceps dip", "Push-up and rotation", "Side plank"]);
-  });
-
-  it("will input the user's desired workout focus and output a relevant regimen", function() {
-    var newWorkout = new Workout("Core");
-    expect(newWorkout.?????()).to.eql(["Chair step-up", "Squat", "Tree Pose"]);
-  });
-}):
+  // it("will input the available time and equipment and output an exercise", function() {
+  //   var testExercise = new Exercise("Chair");
+  //   expect(testExercise.exerciseChooser()).to.equal("Chair step-up");
+  // });
+  //
+  // it("will input the user's regimen choice and reveal the exercise description", function() {
+  //   var newWorkout = new newExercise("Standard Push-up");
+  //   expect(newWorkout.?????()).to.eql(["Places arms below shoulders in plank position, lower your body, keeping core engaged and body in a straight line, push back up to plank"]);
+  // });
+  //
+  // it("will input the user's name and greet them with said name", function() {
+  //   var newWorkout = new Workout("Torrence");
+  //   expect(newWorkout.userName).to.equal("Torrence");
+  // });
+  //
+  // it("will input available time and output number of exercises", function() {
+  //   var newWorkout = new Workout(10);
+  //   expect(newWorkout.????()).to.equal(5);
+  // });
+  //
+  // it("will input level of difficulty and output a matching regimen", function() {
+  //   var newWorkout = new Workout("Advanced");
+  //   expect(newWorkout.????()).to.eql(["Triceps dip", "Push-up and rotation", "Side plank"]);
+  // });
+  //
+  // it("will input the user's desired workout focus and output a relevant regimen", function() {
+  //   var newWorkout = new Workout("Core");
+  //   expect(newWorkout.?????()).to.eql(["Chair step-up", "Squat", "Tree Pose"]);
+  // });
+// });
